d98dbe9443a0d5fb12f6879e256b563cd22e6fb5
SoutaMy <c0a221316d@edu.teu.ac.jp>
2023-10-03 16:28:28 +0900

    en2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,106 +1,109 @@
-import random
 import sys
 import pygame as pg
+import random
 
 WIDTH, HEIGHT = 1100, 600
-
-delta = {
-    pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, +5),
-    pg.K_LEFT: (-5, 0),
-    pg.K_RIGHT: (+5, 0),
-}
+BOMB_RADIUS = 10
+BOMB_COLOR = (255, 0, 0)  # 赤色
 
 
-def check_bound(obj_rct: pg.Rect):
-    yoko, tate = True, True
-    if obj_rct.left < 0 or WIDTH < obj_rct.right:
-        yoko = False
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
-        tate = False
-    return yoko, tate
+def is_inside_screen(rect):
+    return (0 <= rect.left and rect.right <= WIDTH, 0 <= rect.top and rect.bottom <= HEIGHT)
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = (900, 400)
-
-    bd_img = pg.Surface((20, 20))
-    bd_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_rct = bd_img.get_rect()
-    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bd_rct.center = (x, y)
-    vx, vy = +5, +5
-
-    kk_direction = (0, -5)  # 初期方向
-
-    kk_images = {
-        (0, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, 90, 1.0), True, False),  # Down
-        (0, 5): pg.transform.rotozoom(kk_img, 90, 1.0),  # Up
-        (-5, 0): pg.transform.rotozoom(kk_img, 0, 1.0),  # Right
-        (5, 0): pg.transform.flip(pg.transform.rotozoom(kk_img, 0, 1.0), True, False),  # Left
-        (-5, -5): pg.transform.rotozoom(kk_img, -45, 1.0),  # Bottom Right
-        (5, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, -45, 1.0), True, False),  # Bottom Left
-        (-5, 5): pg.transform.rotozoom(kk_img, 45, 1.0),  # Top Right
-        (5, 5): pg.transform.flip(pg.transform.rotozoom(kk_img, 45, 1.0), True, False),  # Top Left
-    }
-
-
-
+    kk_rect = kk_img.get_rect(topleft=(900, 400))
     clock = pg.time.Clock()
     tmr = 0
+
+    KEY_MOVEMENTS = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0)
+    }
+    # 各方向に対するこうかとんの画像を作成
+    kk_images = {
+    (0, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, 90, 1.0), True,True ),  # 上向き
+    (0, 5): pg.transform.rotozoom(kk_img, 90, 1.0),   # 下向き
+    (-5, 0): pg.transform.rotozoom(kk_img, 0, 1.0),  # 右向き
+    (5, 0): pg.transform.flip(pg.transform.rotozoom(kk_img, 0, 1.0), True, False),  # 左向き
+    (-5, -5): pg.transform.rotozoom(kk_img, -45, 1.0),  # 右下斜め
+    (5, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, -45, 1.0), True, False), # 左下斜め
+    (-5, 5): pg.transform.rotozoom(kk_img, 45, 1.0), # 右上斜め
+    (5, 5): pg.transform.flip(pg.transform.rotozoom(kk_img, 45, 1.0), True, False), # 左上斜め
+    }
+    # 爆弾Surfaceの作成
+    bomb_surface = pg.Surface((20, 20))
+    bomb_surface.fill((0, 0, 0))  # 黒で塗りつぶす
+    pg.draw.circle(bomb_surface, BOMB_COLOR, (10, 10), 10)  # 赤い円を描画
+    bomb_surface.set_colorkey((0, 0, 0))  # 黒を透明にする
+
+    # 爆弾Rectのランダムな位置を設定
+    bomb_rect = bomb_surface.get_rect()
+    bomb_rect.topleft = (random.randint(0, WIDTH - 20), random.randint(0, HEIGHT - 20))
+
+    # 加速度のリストを作成
+    accs = [a for a in range(1, 11)]
+
+    # 拡大爆弾Surfaceのリストを作成
+    bb_imgs = []
+    bomb_rects = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r), pg.SRCALPHA)
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+        bomb_rects.append(bb_img.get_rect())
+    
+    # 爆弾の速度を設定
+    vx, vy= 5, 5
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT: 
                 return
 
-        if kk_rct.colliderect(bd_rct):
-            print("ゲームオーバー")
-            return
+        key_lst = pg.key.get_pressed()
+        dx, dy = 0, 0
+        for key, (vx_key, vy_key) in KEY_MOVEMENTS.items():
+            if key_lst[key]:
+                dx += vx_key
+                dy += vy_key
+        kk_rect.move_ip(dx, dy)
+        inside_x, inside_y = is_inside_screen(kk_rect)
+        if not inside_x:
+            kk_rect.move_ip(-dx, 0)
+        if not inside_y:
+            kk_rect.move_ip(0, -dy)
+
+        # 爆弾の移動
+        avx, avy = vx * accs[min(tmr // 500, 9)], vy * accs[min(tmr // 500, 9)]
+        bomb_rect.move_ip(avx, avy)
+        inside_x, inside_y = is_inside_screen(bomb_rect)
+        if not inside_x:
+            vx = -vx  # 速度の符号を反転
+        if not inside_y:
+            vy = -vy  # 速度の符号を反転
+
+        # 爆弾の大きさを変更
+        bomb_rect.size = bomb_rects[min(tmr // 500, 9)].size
+        bomb_surface = bb_imgs[min(tmr // 500, 9)]  # 爆弾のサイズを更新
+
+        # こうかとんと爆弾が衝突したかどうかを判定
+        if kk_rect.colliderect(bomb_rect):
+            return # 衝突した場合、main関数からreturnする
 
         screen.blit(bg_img, [0, 0])
-
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        for key, mv in delta.items():
-            if key_lst[key]:
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
-
-        if sum_mv != [0, 0]:
-            # キーの値に応じて kk_direction を更新
-            kk_direction = (sum_mv[0], sum_mv[1])
-
-        kk_rct.move_ip(sum_mv[0], sum_mv[1])
-
-        if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-
-        # Flip the image if moving down
-        if kk_direction == (0, 5):
-            kk_img = pg.transform.flip(kk_images[kk_direction], False, True)
-        else:
-            kk_img = kk_images[kk_direction]
-
-        screen.blit(kk_img, kk_rct.topleft)
-        
-        bd_rct.move_ip(vx, vy)
-        yoko, tate = check_bound(bd_rct)
-        if not yoko:
-            vx *= -1
-        if not tate:
-            vy *= -1
-        screen.blit(bd_img, bd_rct)
-
+        # 押下されたキーに応じて、適切な画像を選択して表示
+        screen.blit(kk_images.get((dx, dy), kk_img), kk_rect.topleft)
+        screen.blit(bomb_surface, bomb_rect.topleft)  # 爆弾の表示
         pg.display.update()
         tmr += 1
-        clock.tick(50)
+        clock.tick(50)  # FPSを50に変更
 
 if __name__ == "__main__":
     pg.init()
