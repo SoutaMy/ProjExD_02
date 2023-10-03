bffa4b7278ecc9e173b9a387fe923baf9122ba5a
SoutaMy <c0a221316d@edu.teu.ac.jp>
2023-10-03 13:49:31 +0900

    1to2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,8 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1600, 900
-
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -13,17 +12,46 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+
+    # 爆弾の初期位置と速度を設定
+    bomb_radius = 10
+    bomb_color = (255, 0, 0)
+    bomb_x = random.randint(bomb_radius, WIDTH - bomb_radius)
+    bomb_y = random.randint(bomb_radius, HEIGHT - bomb_radius)
+    bomb_vx = 5
+    bomb_vy = 5
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
 
+        # 背景とキャラクターの描画
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+
+        # 爆弾の描画
+        bomb_surface = pg.Surface((2 * bomb_radius, 2 * bomb_radius), pg.SRCALPHA)
+        pg.draw.circle(bomb_surface, bomb_color, (bomb_radius, bomb_radius), bomb_radius)
+        screen.blit(bomb_surface, (bomb_x - bomb_radius, bomb_y - bomb_radius))
+
+        # 爆弾の位置を更新
+        bomb_x += bomb_vx
+        bomb_y += bomb_vy
+
+        # 画面端で反対側に移動させる
+        if bomb_x < -bomb_radius:
+            bomb_x = WIDTH + bomb_radius
+        elif bomb_x > WIDTH + bomb_radius:
+            bomb_x = -bomb_radius
+        if bomb_y < -bomb_radius:
+            bomb_y = HEIGHT + bomb_radius
+        elif bomb_y > HEIGHT + bomb_radius:
+            bomb_y = -bomb_radius
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
-
+        clock.tick(50)  # FPSを50に設定
 
 if __name__ == "__main__":
     pg.init()
